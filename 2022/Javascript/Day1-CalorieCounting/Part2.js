8734bc7f8a0ad4063cff6cfad7ac86f88a0c11bb
TheBluePineapple <85315963+TheBluePineapple@users.noreply.github.com>
2022-12-05 16:35:45 -0500

    Create Part2.js

---
+++ 2022/Javascript/Day1-CalorieCounting/Part2.js
@@ -0,0 +1,17 @@
+let a = await fetch("https://adventofcode.com/2022/day/1/input").then(res=>res.text()).then(data=>{
+    let most = [0,0,0];
+    data.split('\n\n').forEach(elf=>{
+        let calories = 0;
+        elf.split('\n').forEach(snack=>{
+            calories+=parseInt(snack);
+        })
+        for(let i = 0; i<most.length; i++){
+            if(calories>most[i]){
+                most.splice(i,0,calories);
+                most.pop();
+                return;
+            }
+        }
+    })
+    return most[0]+most[1]+most[2];
+}).catch(e=>console.error(e));
